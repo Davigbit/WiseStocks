15926c32f49b1e452548ae1f400e207bd67732b4
Your Name <your_email@example.com>
2024-04-06 07:36:14 -0400

    Add of percentage change

--- json_creator.py
+++ json_creator.py
@@ -7,6 +7,9 @@ DATASET = {}
 for column_name in ['NA', 'SU', 'BCE', 'IMO']:
     X,Y = values_curve_7_days(column_name)
     first_30_days = joblib.load(f'./pkls/first_30_days_{column_name}.pkl')
+    yesterday_value = first_30_days[1]
+    change = joblib.load(f'./pkls/todaychange_{column_name}.pkl')
+    percentage_variation = change*100/yesterday_value
     first_30_days.index = first_30_days.index.astype(str)
     days = first_30_days.index.tolist()
     first_30_days = first_30_days.to_list()
@@ -16,7 +19,7 @@ for column_name in ['NA', 'SU', 'BCE', 'IMO']:
     dict = {'thirty_days': [days, first_30_days],
             'seven_days': [X,Y],
             'today_value': joblib.load(f'./pkls/todayvalue_{column_name}.pkl'),
-            'today_change': joblib.load(f'./pkls/todaychange_{column_name}.pkl')}
+            'today_change': percentage_variation}
     DATASET[f'{column_name}'] = dict
 DATASET = json.dumps(DATASET)
 
